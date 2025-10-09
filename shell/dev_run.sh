72c7a7d8b8b0e886f638c6f58fb16cfdb6b32792
nihongshuai <nihongshuai@ushareit.com>
2025-10-09 17:15:04 +0800

    update

--- shell/dev_run.sh
+++ shell/dev_run.sh
@@ -5,10 +5,10 @@ env_flag_file=conf/test.conf
 echo "server_path: ${server_path}"
 echo "env_flag_file: ${env_flag_file}"
 echo "env_flag_file: ${env_flag_file}"
-#killall access_brpc 
-#for model_adapter_pid in "`ps -ef | grep access_brpc | awk '{print $2}'| sed -n '1'p`"
+#killall lbs_brpc 
+#for model_adapter_pid in "`ps -ef | grep lbs_brpc | awk '{print $2}'| sed -n '1'p`"
 #do
 #kill -9 $model_adapter_pid;
 #done
 
-${server_path}/access_brpc --flagfile=${env_flag_file}
+${server_path}/lbs_brpc --flagfile=${env_flag_file}
